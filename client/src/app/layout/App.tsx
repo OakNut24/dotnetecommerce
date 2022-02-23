a2b4e36191811dd4803ea741a97f36f0d73461ef
Alon Ben Yaakov <alonbenyaakov24@gmail.com>
2022-02-23 20:28:38 +0200

    Section Video 43

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -3,6 +3,11 @@ import { Container, CssBaseline } from "@mui/material";
 import Catalog from "../../features/catalog/catalog";
 import Header from "./Header";
 import { useState } from 'react';
+import { Route } from 'react-router-dom';
+import HomePage from '../../features/home/HomePage';
+import ProductDetails from '../../features/catalog/ProductDetails';
+import AboutPage from '../../features/about/AboutPage';
+import ContactPage from '../../features/contact/ContactPage';
 
 
 function App() {
@@ -29,7 +34,12 @@ function App() {
         <CssBaseline />
         <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
         <Container>
-          <Catalog />
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/catalog" component={Catalog} />
+          <Route path="/catalog/:id" component={ProductDetails} />
+          <Route path="/about" component={AboutPage} />
+          <Route path="/contact" component={ContactPage} />
+
         </Container>
       </ThemeProvider>
     </>
---
+++ client/src/features/catalog/ProductDetails.tsx
@@ -0,0 +1,6 @@
+import { Typography } from "@mui/material";
+
+export default function ProductDetails() {
+    return <><Typography variant='h2'>
+        ProductDetails</Typography> </>
+}
